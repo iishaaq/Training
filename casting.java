bc59c8e2dbf6d66e2641f41fdc0bfa039a3de8d3
iishaaq <36529006+iishaaq@users.noreply.github.com>
2019-03-20 19:49:11 +0530

    Casting

---
+++ casting.java
@@ -0,0 +1,16 @@
+package Variables;
+
+public class casting {
+
+	public static void main(String[] args) {
+		byte x = 125;
+		byte y = 5;
+		byte Z = (byte)(x+y);
+		System.out.println(Z);
+		
+//		for(double i =10; i!=0; i-=.1) {
+//		System.out.println(i);
+//		}
+	}
+
+}
